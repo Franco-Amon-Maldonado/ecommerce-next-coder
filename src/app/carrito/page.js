b803bd57a81976d2dd922ae1b06b7dff0cbb48d9
Franco Amon Maldonado <franco.m_887@outlook.com>
2024-01-29 22:29:19 -0300

    Pagina carrito de compras Svg carrito de compras

---
+++ src/app/carrito/page.js
@@ -0,0 +1,5 @@
+const Carrito = () => {
+	return <h1 className="text-4xl text-center">Carrito de compras</h1>
+}
+
+export default Carrito
---
+++ src/components/CarritoSvg.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const CarritoSvg = () => {
+	return (
+		<svg
+			
+			className=""
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			stroke-width="2"
+			stroke="currentColor"
+			fill="none"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		>
+			<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+			<path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+			<path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+			<path d="M17 17h-11v-14h-2" />
+			<path d="M6 5l14 1l-1 7h-13" />
+		</svg>
+	)
+}
+
+export default CarritoSvg
