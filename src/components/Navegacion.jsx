e3185cd9862e834e64c0128e4e16e4f985c48e5b
Franco Amon Maldonado <franco.m_887@outlook.com>
2024-01-29 22:29:34 -0300

    Modificacion navegacion

--- src/components/Navegacion.jsx
+++ src/components/Navegacion.jsx
@@ -1,9 +1,10 @@
-import DropMenu from "./DropMenu"
+import CarritoSvg from './CarritoSvg'
+import DropMenu from './DropMenu'
 
 const Navegacion = () => {
 	return (
 		<nav className="bg-white border-gray-200 dark:bg-gray-900">
-			<div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+			<div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-3">
 				<a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
 					{/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" /> */}
 					{/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span> */}
@@ -83,7 +84,11 @@ const Navegacion = () => {
 							/>
 						</svg>
 					</button>
+					<span className=" ml-5 self-center">
+						<CarritoSvg />
+					</span>
 				</div>
+
 				<div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
 					<div className="relative mt-3 md:hidden">
 						<div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -124,12 +129,10 @@ const Navegacion = () => {
 							<a
 								href="#"
 								className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-							>
-                                
-							</a>
+							></a>
 						</li>
 						<li>
-							<DropMenu/>
+							<DropMenu />
 						</li>
 					</ul>
 				</div>
