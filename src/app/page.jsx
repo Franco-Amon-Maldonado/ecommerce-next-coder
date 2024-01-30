97e78e78725f6684ec527abb63a9c52f574910d9
Franco Amon Maldonado <franco.m_887@outlook.com>
2024-01-29 22:28:39 -0300

    Card mokeadas

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,27 @@
-
+import CardProducto from '@/components/CardProducto'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen p-24">
-      <h1>Soy el home</h1>
-    </main>
-  );
+	return (
+		<main className="min-h-screen max-w-7xl m-auto bg-white">
+
+			<section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 place-items-center gap-5 mt-10">
+				<article>
+					<CardProducto />
+				</article>
+
+				<article>
+					<CardProducto />
+				</article>
+
+				<article>
+					<CardProducto />
+				</article>
+
+				<article>
+					<CardProducto />
+				</article>
+			</section>
+      
+		</main>
+	)
 }
